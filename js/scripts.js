a48f2cdf16f90aea727341a40d51abd462e9ef03
Morgan <morganhondros@gmail.com>
2017-10-11 21:29:08 -0400

    commiting files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$("document").ready(function(){
+
+$("body").append("<h1>This is a heading</h1>");
+
+
+});
+
+$("document").ready(function(){
+
+$("body").append("<h1>This is our fist code</h1>");
+
+});
